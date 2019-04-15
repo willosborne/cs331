c6cc6a8078e9f9b4d3677640ff90f7575ee41e76
Will Osborne <willfosborne@gmail.com>
2019-04-15 18:26:47 +0100

    Stuff for release

---
+++ README.md
@@ -0,0 +1,17 @@
+# CS331 Neural Computing
+A simple Multi-Layer Perceptron solving the XOR problem. Nothing fancy, but brought some interesting challenges with it.
+
+If you're reading this, you should be on the right branch for DCS stuff.
+Requirements:
+ - Python 2.7 (**not** 3.x, that will not work)
+ - pandas
+ - numpy
+ - sklearn
+ - tensorflow
+ - keras
+ 
+ Any sane Python should have `pip` - just use `pip install ...` on each package.
+ 
+ Many thanks!
+ 
+ Will Osborne
--- xor-mlp.py
+++ xor-mlp.py
@@ -1,7 +1,18 @@
+# CS331 Neural Computing coursework
+# Will Osborne - u1603746
+# willfosborne@gmail.com
+
+
+# all this stuff needs to be installed:
+# numpy
+# pandas
+# sklearn
+# tensorflow
+# keras
+# install it with pip install x, or python -m pip install if that doesn't work
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import SGD
-# from keras.losses import mean_squared_error
 
 import numpy as np
 import pandas as pd
@@ -11,43 +22,39 @@ import matplotlib.cm as cm
 
 from sklearn.metrics import mean_squared_error
 
-# import seaborn as sns
-# sns.set()
-
-# C-c C-r f - fix indentation automatically
-
-# base_X = [[0, 0, 1, 1],
-#           [0, 1, 0, 1],
-#           [-1, -1, -1, -1]]
 base_X_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
 base_t_data = [0, 1, 1, 0]
 
-
 def generate_training_data(n):
     print "Generatic training data..."
+    # dataframe of gaussian noise
     noise = pd.DataFrame(np.random.normal(loc=0.0, scale=0.5, size=(n * 4, 2)))
-    # print noise
+    # add to base data and duplicate
     X = pd.DataFrame(base_X_data * n) + noise
+    # add offset
     X["offset"] = [-1] * n * 4
 
     t = pd.DataFrame(base_t_data * n)
     X["t"] = t
 
+    # fix columns
     X.columns = ["x", "y", "offset", "t"]
-
-    # print X
 
     return X
 
 
 def display_accuracy_image(model, hidden_neurons, training_vectors):
+    """Take a model and control parameters, and display the mapping as an image."""
     title = "{} hidden neurons, {} training vectors".format(hidden_neurons, training_vectors)
     filename = "images/image_{}neurons_{}vectors.png".format(hidden_neurons, training_vectors)
 
     splits = 30
 
+
+    # generate an evenly-spaced 2D grid
     r = np.linspace(0, 1, splits, endpoint=True)
 
+    # make a DataFrame of it for testing
     df = pd.DataFrame(columns=["x", "y"])
     for y in r:
         df2 = pd.DataFrame()
@@ -56,20 +63,16 @@ def display_accuracy_image(model, hidden_neurons, training_vectors):
         df = pd.concat([df, df2])
     df["offset"] = -1
 
+    # predict each x,y pair
     output = model.predict(np.array(df), batch_size=1)
     df["out"] = output
 
     del df["offset"]
-
-    # print df
-   
     del df["x"], df["y"]
 
     plt.figure()
 
-    # ax = fig.add_axes([0, 1, 0, 1])
-    # ax = fig.
-    # ax.invert_yaxis()
+    # dump the grey values into an image and display it
     plt.imshow(
         df.values.reshape(splits, splits),
         cmap=cm.Greys_r,
@@ -80,11 +83,13 @@ def display_accuracy_image(model, hidden_neurons, training_vectors):
     plt.savefig(filename)
 
 def display_accuracy_epoch_graph(history, hidden_neurons, training_vectors):
+    """Take a model and control data, and display the loss/epoch data from the fitting of that model"""
     title = "{} hidden neurons, {} training vectors".format(hidden_neurons, training_vectors)
     filename = "images/loss_{}neurons_{}vectors.png".format(hidden_neurons, training_vectors)
 
     plt.figure()
     
+    # just plot it, nothing fancy here
     plt.plot(history.history["loss"])
     plt.title(title)
 
@@ -95,25 +100,29 @@ def display_accuracy_epoch_graph(history, hidden_neurons, training_vectors):
 
 
 def build_model(hidden_neurons):
+    # use a Sequential model - nothing fancy
     model = Sequential()
+    # input dimension is 3 to account for the offset column
     model.add(Dense(hidden_neurons, input_dim=3))
-    model.add(Activation("sigmoid"))
-    model.add(Dense(1))
-    model.add(Activation("sigmoid"))
+    model.add(Activation("sigmoid")) # activation func for first layer
+    model.add(Dense(1)) # output layer
+    model.add(Activation("sigmoid")) # ...and its activation func
 
+    # compile with SGD, mean sq error and accuracy
     model.compile(optimizer=SGD(lr=0.1),
                   loss="mean_squared_error",
                   metrics=["acc"])
 
     return model
 
-
-write_predictions = False
+# can turn off the dumping of accuracies here
+write_predictions = True
 
 
 def run():
     f = None
 
+    # make test pattern
     test_patterns = generate_training_data(64)
     ideal_predictions = test_patterns["t"]
     del test_patterns["t"]
@@ -121,33 +130,33 @@ def run():
     if write_predictions:
         f = open("prediction_accuracy.txt", "w")
     
+    # for each combo of size and neuron count
     for data_size in [16, 32, 64]:
         for hidden_neurons in [2, 4, 8]:
             title = "{} hidden neurons, {} training vectors".format(hidden_neurons, data_size)
             print title
 
+            # make a model as appropriate and data as appropriate
             model = build_model(hidden_neurons)
             X = generate_training_data(data_size)
+            # alternatively load pre-generated data
             # X = pd.read_csv("data_" + str(data_size) + "_good.csv")
-            # X = X.reindex(columns=['x', 'y', 'offset', 't'])
-            # X.fillna(0, inplace=True)
 
-            print X
-
+            # extract target
             t = X["t"]
             del X["t"]
-            print t
-            # t.columns = t.columns.to_series().apply(lambda x: x.strip())
 		
+            # fit the model to the data
+            # np.array stuff is for compatibility w/ dcs versions of stuff
+            # batch_size must be 1 or it behaves very differently - one vector at a time, please
+            # 1000 epochs
+            history = model.fit(np.array(X), np.array(t), nb_epoch=1000, batch_size=1)
 
-            history = model.fit(np.array(X), np.array(t), nb_epoch=10, batch_size=1)
-
-            # display_accuracy_image(model, title)
             display_accuracy_image(model, hidden_neurons, data_size)
             display_accuracy_epoch_graph(history, hidden_neurons, data_size)
 
             if write_predictions:
-
+                # dump predictions
                 predictions = model.predict(np.array(test_patterns), batch_size=1)
 
                 error = mean_squared_error(ideal_predictions, predictions)
@@ -161,20 +170,7 @@ def run():
     if write_predictions:
         f.close()
 
-    # display_accuracy_epoch_graph(history)
-    # model = build_model(8)
-    # n = 16
-    # X = generate_training_data(n)
-    # X.to_csv("data_" + str(n) + ".csv", header=True, index=False)
-    # X = pd.read_csv("data_64_good.csv")
-    # print X
-    # t = X["t"]
-    # del X["t"]
-
-    # history = model.fit(X, t, epochs=128, batch_size=1)
-
-    # display_accuracy_image(model)
-    # # display_accuracy_epoch_graph(history)
-
-
 run()
+
+# that's all, folks!
+# this was a fun coursework! many thanks :D
